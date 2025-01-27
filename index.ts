e0489c7929c9dcac5990ab76707386c546b7bc41
RelixOfficial <relixofficial4@gmail.com>
2025-01-27 07:05:52 +0700

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,50 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+import moment from "moment";
+import { globalErrorMiddleware } from "src/middlewares/error.middleware.ts";
+import { notFoundMiddleware } from "src/middlewares/notfound.middleware.ts";
+import { serve } from "@hono/node-server";
+import { env } from "src/env";
+import { createSessionController } from "src/controllers/session";
+import * as whastapp from "wa-multi-session";
+import { createMessageController } from "src/controllers/message";
+
+const app = new Hono();
+
+app.use(
+  logger((...params) => {
+    params.map((e) => console.log(`${moment().toISOString()} | ${e}`));
+  })
+);
+app.use(cors());
+
+app.onError(globalErrorMiddleware);
+app.notFound(notFoundMiddleware);
+
+/**
+ * session routes
+ */
+app.route("/session", createSessionController());
+/**
+ * message routes
+ */
+app.route("/message", createMessageController());
+
+const port = process.env.PORT || env.PORT;
+
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
+
+whastapp.onConnected((session) => {
+  console.log(`session: '${session}' connected`);
+});
+
+whastapp.loadSessionsFromStorage();
